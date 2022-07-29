14353a2c7a061b3f6e4ca71bfc1a79755a06b73d
rthandi <robbie.thandi@gmail.com>
2022-07-29 16:18:21 +0100

    Add git style calendar

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,53 @@
 import React from "react";
-import Dashboard from "./Dashboard/Dashboard";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import CalendarComponent from "./Calendar/CalendarContent";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
+// import Dashboard from "./Dashboard/Dashboard";
+// import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+// import CalendarComponent from "./Calendar/CalendarContent";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import {ResponsiveCalendar} from '@nivo/calendar';
+import {data} from './data';
 
 function App() {
-  const mdTheme = createTheme({
-    palette: {
-      mode: "light",
-    },
-  });
-
-  require("react-big-calendar/lib/css/react-big-calendar.css");
+  // const mdTheme = createTheme({
+  //   palette: {
+  //     mode: "light",
+  //   },
+  // });
 
   return (
-    <ThemeProvider theme={mdTheme}>
-      <div className="App">
-        <Router>
-          <Routes>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/Dashboard" element={<Dashboard />} />
-            <Route path="/Calendar" element={<CalendarComponent />} />
-          </Routes>
-        </Router>
-      </div>
-    </ThemeProvider>
+    <div className="App">
+      {/*<Router>*/}
+      {/*  <Routes>*/}
+      {/*    <Route path="/" element={<Dashboard />} />*/}
+      {/*    <Route path="/Dashboard" element={<Dashboard />} />*/}
+      {/*    <Route path="/Calendar" element={<CalendarComponent />} />*/}
+      {/*  </Routes>*/}
+      {/*</Router>*/}
+      <ResponsiveCalendar
+        data={data}
+        from="2015-03-01"
+        to="2016-07-12"
+        emptyColor="#eeeeee"
+        colors={[ '#61cdbb', '#97e3d5', '#e8c1a0', '#f47560' ]}
+        margin={{ top: 40, right: 40, bottom: 40, left: 40 }}
+        yearSpacing={40}
+        monthBorderColor="#ffffff"
+        dayBorderWidth={2}
+        dayBorderColor="#ffffff"
+        legends={[
+          {
+            anchor: 'bottom-right',
+            direction: 'row',
+            translateY: 36,
+            itemCount: 4,
+            itemWidth: 42,
+            itemHeight: 36,
+            itemsSpacing: 14,
+            itemDirection: 'right-to-left'
+          }
+        ]}
+      />
+    </div>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
+import * as ReactDOM from 'react-dom';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+//
+// const root = createRoot(
+//   document.getElementById('root') as HTMLElement
+// );
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
+  const root = <React.StrictMode>
     <meta name="viewport" content="initial-scale=1, width=device-width" />
     <App />
   </React.StrictMode>
-);
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
